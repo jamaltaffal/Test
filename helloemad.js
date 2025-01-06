643c9a065e7168a2876223c2cfc40ad01c367e0e
jamaltaffal <jamal.taffal@optimumpartners.co>
2025-01-06 15:56:08 +0300

    Update helloemad.js

--- helloemad.js
+++ helloemad.js
@@ -1,19 +1,14 @@
 // Import the built-in HTTP module
 const http = require('http');
 
-// Define the port to listen on
-const port = 3000;
-
 // Create the HTTP server
-const server = http.createServer((req, res) => {
-  // Set the response HTTP header
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
+const server = http.createServer((req, res) =>{
 
   // Send a response to the client
   res.end('Hello from Emad!');
 });
 
 // Start the server and listen on the specified port and IP
-server.listen(port 3000, '0.0.0.0', () => {
-  console.log(`Server is running on port ${port}`);
+server.listen(3000,() => {
+  console.log(`Server is running on port 3000`);
 });
