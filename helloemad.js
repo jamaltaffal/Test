a5a8fc3338501f1547a8c84df7767eddf0e16bd1
jamaltaffal <jamal.taffal@optimumpartners.co>
2025-01-06 15:18:29 +0300

    Update helloemad.js

--- helloemad.js
+++ helloemad.js
@@ -4,7 +4,7 @@
 console.log("Hello Emad");
 
 // Log the port number to the console (e.g., for a web server)
-const port = 4444; // Example port number
+const port = 3000; // Example port number
 console.log(`Server is running on port: ${port}`);
 
 
